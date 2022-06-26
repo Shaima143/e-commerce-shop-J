bdd6225816d4e02134896e670bf98628358559f9
Shaima Al Shaibani <shaimaalshaibani@Shaimas-MacBook-Pro.local>
2022-06-26 09:56:41 +0400

    Added logout link in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,17 @@
                 </div>
             </div>
         </div>
+
+        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            Logout
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+        </form>
+
+
     </div>
+
 </div>
 @endsection
